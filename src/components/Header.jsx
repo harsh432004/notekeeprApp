47459f90aaa35e633cce38e68c7105a29c155d28
Harsh  Vaidya <harshvaidya432004@gmail.com>
2023-08-12 15:19:48 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        Mann Ki Baat{" "}
+        <a href="https://www.instagram.com/harsh._.vaidya/">Harsh</a> Ke Saath
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
